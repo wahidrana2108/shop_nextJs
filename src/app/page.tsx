2c63a098401e4c69c81f2c7c783b659aa1859560
wahidrana2108 <wahidzamanrana@gmail.com>
2024-03-18 19:36:16 +0600

    testimonials updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import NewProducts from "@/components/NewProducts";
+import Testimonials from "@/components/Testimonials";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
        <Hero />
        <NewProducts />
+       <Testimonials />
     </main>
   );
 }
